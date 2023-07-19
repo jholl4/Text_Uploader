c85c541b27514fd678d2fa337c36e1dc2010540f
Joshua Holland <joshua.holland.1@outlook.com>
2023-07-19 13:53:47 -0400

    Removed the comments from the Coursera lesson and added my own. Got rid of the list-making function and simplified that to one line. Moved the post() definition up and renamed make_dict() to uploader() while making some changes inside, including a call to the post() function to upload each dictionary once it's created and before continuing to the next and overwriting it.

--- run.py
+++ run.py
@@ -3,40 +3,36 @@
 import os
 import requests
 
-ip_address = 
-src_dir = "/data/feedback"
+# ip address of the web server should be added here
+ip_address = ""
+# This is the directory where all of the text files with the feedback are stored
+src_dir = "/data/feedback/"
+# Keys to use for the temporary dictionaries in the uploader() function
 data_keys = ['title', 'name', 'date', 'feedback']
+# Add the files in the source directory to a list to be iterated through in the uploader() function
+feedback_list = os.listdir(src_dir)
 
-# List all .txt files under /data/feedback directory that contains the actual feedback to be displayed on the company's website.
-# Hint: Use os.listdir() method for this, which returns a list of all files and directories in the specified path.
-def make_file_list():
-    feedback_list = []
-    for file in os.listdir(src_dir):
-        feedback_list.append(file)
-    return(feedback_list)
-
-# You should now have a list that contains all of the feedback files from the path /data/feedback. Traverse over each file and, from
-# the contents of these test files, create a dictionary by keeping title, name, date, and feedback as keys for the content value, respectively.
-def make_dict():
-    data_dict = {}
-    for file in make_file_list():
-        with open(src_dir + file, 'r') as f:
-            lines = f.readlines()
-            for key, line in zip(data_keys, lines):
-                data_dict.update({key: line})
-    return data_dict
-                
-
-
-# Now, you need to have a dictionary with keys and their respective values (content from feedback files). This will be uploaded through the Django REST API.
-# Use the Python requests module to post the dictionary to the company's website. Use the request.post() method to make a POST request to:
-# http://<corpweb-external-IP>/feedback. Replace <corpweb-external-IP> with corpweb's external IP address.
-def post()
-    resp = requests.post("http://" + ip_address + "/feedback/", json = make_dict())
+# Define the post function to send the POST to the server
+def post(dict: dict):
+    resp = requests.post("http://" + ip_address + "/feedback/", json = dict)
     # Make sure an error message isn't returned. You can print the status_code and text of the response objects to check out what's going on.
     # You can also use the response status_code 201 for created success status response code that indicates the request has succeeded.
     print(resp.raise_for_status())
     print(resp.status_code)
 
+# Define the uploader, which will call the post() function at the end.
+def uploader():
+    # Iterate through the list of files in the source directory
+    for file in feedback_list:
+        # Open each file, read the lines of the file and add them into a temporary dictionary, then upload them to the web service vie the post() function
+        with open(src_dir + file, 'r') as f:
+            data_dict = {}
+            lines = f.readlines()
+            for key, line in zip(data_keys, lines):
+                data_dict.update({key: line})
+                print(f"Data: {data_dict}")
+            post(data_dict)
+
+
 if __name__ == "__main__":
-    post()
+    uploader()
